a90f1a64a30c28460d1fca1f63c2bff9d60bc8ba
Kr3pt0n <broni.dryg.pony@gmail.com>
2023-04-19 22:14:40 +0600

    local

--- app/src/main/java/com/example/listeningeverywhere/ui/viewmodels/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/listeningeverywhere/ui/viewmodels/fragments/HomeFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.widget.ProgressBar
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.listeningeverywhere.R
@@ -15,19 +15,15 @@ import com.example.listeningeverywhere.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
-
-    lateinit var  mainViewModel: MainViewModel
-
+    private val mainViewModel: MainViewModel by viewModels()
 
     @Inject
     lateinit var songAdapter: SongAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mainViewModel= ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         setupRecyclerView()
         subscribeToObservers()
 
@@ -36,23 +32,25 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         }
     }
 
-    private fun setupRecyclerView() = requireView().findViewById<RecyclerView>(R.id.rvAllSongs).apply {
-        adapter = songAdapter
-        layoutManager = LinearLayoutManager(requireContext())
-    }
+    private fun setupRecyclerView() =
+        requireView().findViewById<RecyclerView>(R.id.rvAllSongs).apply {
+            adapter = songAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
 
     private fun subscribeToObservers() {
-        mainViewModel.mediaItems.observe(viewLifecycleOwner){result ->
-            when(result.status){
+        mainViewModel.mediaItems.observe(viewLifecycleOwner) { result ->
+            when (result.status) {
                 Status.SUCCESS -> {
-                      requireView().findViewById<ProgressBar>(R.id.allSongsProgressBar).isVisible = false
-                    result.data?.let {
-                        songs ->
+                    requireView().findViewById<ProgressBar>(R.id.allSongsProgressBar).isVisible =
+                        false
+                    result.data?.let { songs ->
                         songAdapter.songs = songs
                     }
                 }
                 Status.ERROR -> Unit
-                Status.LOADING ->  requireView().findViewById<ProgressBar>(R.id.allSongsProgressBar).isVisible = true
+                Status.LOADING -> requireView().findViewById<ProgressBar>(R.id.allSongsProgressBar).isVisible =
+                    true
 
             }
         }
