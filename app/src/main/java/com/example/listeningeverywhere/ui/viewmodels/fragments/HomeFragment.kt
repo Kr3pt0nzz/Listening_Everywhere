0e285d4816a59b2e77ed59792430919bc06edeef
Kr3pt0n <broni.dryg.pony@gmail.com>
2023-04-19 15:40:28 +0600

    .

--- app/src/main/java/com/example/listeningeverywhere/ui/viewmodels/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/listeningeverywhere/ui/viewmodels/fragments/HomeFragment.kt
@@ -2,8 +2,12 @@ package com.example.listeningeverywhere.ui.viewmodels.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.listeningeverywhere.R
 import com.example.listeningeverywhere.adapters.SongAdapter
 import com.example.listeningeverywhere.other.Status
@@ -24,21 +28,31 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel= ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        setupRecyclerView()
+        subscribeToObservers()
 
+        songAdapter.setOnItemClickListener {
+            mainViewModel.playOrToggleSong(it)
+        }
     }
 
-    private fun subscribeToObservers(view: View){
+    private fun setupRecyclerView() = requireView().findViewById<RecyclerView>(R.id.rvAllSongs).apply {
+        adapter = songAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun subscribeToObservers() {
         mainViewModel.mediaItems.observe(viewLifecycleOwner){result ->
             when(result.status){
                 Status.SUCCESS -> {
-                    allSongsProgressBar.isVisible = false
+                      requireView().findViewById<ProgressBar>(R.id.allSongsProgressBar).isVisible = false
                     result.data?.let {
                         songs ->
                         songAdapter.songs = songs
                     }
                 }
                 Status.ERROR -> Unit
-                Status.LOADING ->  allSongsProgressBar.isVisible = true
+                Status.LOADING ->  requireView().findViewById<ProgressBar>(R.id.allSongsProgressBar).isVisible = true
 
             }
         }
