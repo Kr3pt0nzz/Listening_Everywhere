65c4cb46f20037e83f51fe50ceaf39f3aab16f5f
Atabayev Muslim <m.atabayev@tamur.kz>
2023-04-24 19:02:48 +0600

    Fix navigation issue

--- app/src/main/java/com/example/listeningeverywhere/ui/viewmodels/MainActivity.kt
+++ app/src/main/java/com/example/listeningeverywhere/ui/viewmodels/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
 import com.example.listeningeverywhere.R
@@ -15,14 +16,15 @@ import com.example.listeningeverywhere.adapters.SwipeSongAdapter
 import com.example.listeningeverywhere.data.entities.Song
 import com.example.listeningeverywhere.exoplayer.isPlaying
 import com.example.listeningeverywhere.exoplayer.toSong
-import com.example.listeningeverywhere.other.Status.*
+import com.example.listeningeverywhere.other.Status.ERROR
+import com.example.listeningeverywhere.other.Status.LOADING
+import com.example.listeningeverywhere.other.Status.SUCCESS
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     private val mainViewModel: MainViewModel by viewModels()
 
     @Inject
@@ -35,29 +37,40 @@ class MainActivity : AppCompatActivity() {
 
     private var playbackState: PlaybackStateCompat? = null
 
+    private lateinit var vpSong: ViewPager2
+    private lateinit var ivPlayPause: ImageView
+    private lateinit var ivCurSongImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         subscribeToObservers()
 
-        findViewById<ViewPager2>(R.id.vpSong).adapter = swipeSongAdapter
+        ivCurSongImage = findViewById<ImageView>(R.id.ivCurSongImage).apply {
+            isVisible = false
+        }
 
-        findViewById<ViewPager2>(R.id.vpSong).registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                if(playbackState?.isPlaying == true){
-                    mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+        vpSong = findViewById<ViewPager2>(R.id.vpSong).apply {
+            adapter = swipeSongAdapter
 
-                } else{
-                    curPlayingSong = swipeSongAdapter.songs[position]
+            registerOnPageChangeCallback(object :
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    if (playbackState?.isPlaying == true) {
+                        mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+                    } else {
+                        curPlayingSong = swipeSongAdapter.songs[position]
+                    }
                 }
-            }
-        })
+            })
+        }
 
-
-        findViewById<ImageView>(R.id.ivPlayPause).setOnClickListener {
-            curPlayingSong?.let {
-                mainViewModel.playOrToggleSong(it, true)
+        ivPlayPause = findViewById<ImageView>(R.id.ivPlayPause).apply {
+            setOnClickListener {
+                curPlayingSong?.let {
+                    mainViewModel.playOrToggleSong(it, true)
+                }
             }
         }
 
@@ -67,30 +80,28 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        findViewById<FragmentContainerView>(R.id.navHostFragment).findNavController().addOnDestinationChangedListener{_, destination, _  ->
-           when(destination.id) {
-               R.id.songFragment -> hideBottomBar()
-               R.id.homeFragment -> showBottomBar()
-               else -> showBottomBar()
-           }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment)
+        val navController = navHostFragment?.findNavController()
+
+        navController?.addOnDestinationChangedListener { _, destination, _ ->
+            val isVisible = when (destination.id) {
+                R.id.songFragment -> false
+                else -> true
+            }
+            toggleBottomBar(isVisible)
         }
     }
 
-    private fun hideBottomBar(){
-        findViewById<ImageView>(R.id.ivCurSongImage).isVisible = false
-        findViewById<ViewPager2>(R.id.vpSong).isVisible = false
-        findViewById<ImageView>(R.id.ivPlayPause).isVisible = false
-    }
-    private fun showBottomBar(){
-        findViewById<ImageView>(R.id.ivCurSongImage).isVisible = true
-        findViewById<ViewPager2>(R.id.vpSong).isVisible = true
-        findViewById<ImageView>(R.id.ivPlayPause).isVisible = true
+    private fun toggleBottomBar(isVisible: Boolean) {
+        vpSong.isVisible = isVisible
+        ivPlayPause.isVisible = isVisible
+        ivCurSongImage.isVisible = isVisible
     }
 
     private fun switchViewPagerToCurrentSong(song: Song) {
         val newItemIndex = swipeSongAdapter.songs.indexOf(song)
         if (newItemIndex != -1) {
-            findViewById<ViewPager2>(R.id.vpSong).currentItem = newItemIndex
+            vpSong.currentItem = newItemIndex
             curPlayingSong = song
         }
     }
@@ -114,19 +125,17 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
         mainViewModel.curPlayingSong.observe(this) {
             if (it == null) return@observe
 
             curPlayingSong = it.toSong()
-            glide.load(curPlayingSong?.imageUrl)
-                .into(findViewById(R.id.ivCurSongImage) ?: return@observe)
+            glide.load(curPlayingSong?.imageUrl).into(ivCurSongImage)
         }
 
         mainViewModel.playbackState.observe(this) {
             playbackState = it
-            findViewById<ImageView>(R.id.ivPlayPause).setImageResource(
-                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
-            )
+            ivPlayPause.setImageResource(if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play)
         }
 
         mainViewModel.networkError.observe(this) {
@@ -137,11 +146,10 @@ class MainActivity : AppCompatActivity() {
                         result.message ?: "An unknown error occured",
                         Snackbar.LENGTH_LONG
                     ).show()
+
                     else -> Unit
                 }
-
             }
         }
-
     }
 }
